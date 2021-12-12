eee43caf71ba545bfcc2cc495f389d922ecf65ae
Adarshkumar14 <1829034@kiit.ac.in>
2021-12-12 14:05:40 +0530

    commit -4
    Signed-off-by: Adarshkumar14 <1829034@kiit.ac.in>

---
+++ ec2.profile.js
@@ -0,0 +1,37 @@
+[
+    {
+        "ServiceName": "EC2",
+        "Name": "DescribeInternetGateways",
+        "Regions": "ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2",
+        "Enabled": true,
+        "PageSize": 18
+      },
+      {
+        "ServiceName": "EC2",
+        "Name": "DescribePlacementGroups",
+        "Regions": "ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2",
+        "Enabled": true,
+        "PageSize": 18
+      },
+      {
+        "ServiceName": "EC2",
+        "Name": "DescribeAddresses",
+        "Regions": "ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2",
+        "Enabled": false,
+        "PageSize": 18
+      },
+      {
+        "ServiceName": "EC2",
+        "Name": "DescribeRegions",
+        "Regions": "ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2",
+        "Enabled": false,
+        "PageSize": 18
+      },
+      {
+        "ServiceName": "EC2",
+        "Name": "DescribeAvailabilityZones",
+        "Regions": "ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2",
+        "Enabled": false,
+        "PageSize": 18
+      }
+]
